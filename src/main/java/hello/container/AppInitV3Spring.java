79e0cda4cca85ca87e281cebf171f265f6f0c7b8
wally <goldenale5500@gmail.com>
2023-11-09 02:45:42 +0900

    스프링 MVC로 서블릿 컨테이너 초기화

---
+++ src/main/java/hello/container/AppInitV3Spring.java
@@ -0,0 +1,24 @@
+package hello.container;
+
+import hello.spring.HelloConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitV3Spring implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        System.out.println("AppInitV3Spring.onStartup");
+
+        // 스프링 컨테이너 생성
+        AnnotationConfigWebApplicationContext annotationConfigApplicationContext = new AnnotationConfigWebApplicationContext();
+        annotationConfigApplicationContext.register(HelloConfig.class);
+
+        // 스프링 디스패처 서블릿 생성, 컨테이너 연결
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(annotationConfigApplicationContext);
+        servletContext.addServlet("dispatcherServletV3", dispatcherServlet)
+                .addMapping("/*");  //이 경로로 들어오면 디스패처 서블릿이 처리하도록 매핑
+    }
+}
