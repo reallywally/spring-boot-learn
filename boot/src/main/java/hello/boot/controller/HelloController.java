baeac81c9015a0290d5e94f669c447d7eb0d6f9e
wally <goldenale5500@gmail.com>
2023-11-13 02:59:28 +0900

    부트 배포 원리 모듈 추가
    - 코드보단 이론 설명 위주

---
+++ boot/src/main/java/hello/boot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hello.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-spring")
+    public String helloSpring() {
+        System.out.println("Hello Spring");
+        return "Hello Spring";
+    }
+}
